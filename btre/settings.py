8fe5d86511037dccb0a08d8d7dab509f92653b39
avichhetri8 <48516639+avichhetri8@users.noreply.github.com>
2021-01-03 00:49:21 +0545

    Update settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'za68lq%_a@(2+@oswfz7vpgo-*ywplwx#x$xs)74v+aoshh5a#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
